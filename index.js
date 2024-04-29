8c5dd9406f044bed5d4c9287f0f1b36816725b6d
Sathish7Kumar <sathishnagarajan1996@gmail.com>
2024-04-29 18:02:27 +0530

    after mongoose

--- index.js
+++ index.js
@@ -1,17 +1,97 @@
 const express = require("express")
+
 const dotenv = require("dotenv").config()
-const bp = require('body-parser')
 // dotenv.config()
+
+const bp = require('body-parser')
+
+const cors = require('cors')
+
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(bp.json())
+app.use(cors())
+
+// url connection
+const dbConnect = async() =>{
+    try {
+        await mongoose.connect(process.env.MONGOURL)
+        console.log('db conected successfully');
+    } catch (error) {
+       console.log("db connection failure"); 
+    }
+}
+
+dbConnect()
+
+// schema 
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    mobileNum : Number
+})
+
+// model
+const userData = mongoose.model("user",userSchema)
+
+// routes : 
+
+// post :
+app.post('/users',async(req,res)=>{
+    try {
+        const {name,email,mobileNum} = req.body
+        const user = await userData.findOne({email})
+        if(user){
+            res.send({message:"user already exists"})
+        }
+        else{
+            const newUser = new userData({name,email,mobileNum})
+            await newUser.save()
+            res.send({message:"user created"})
+        }
+    } catch (error) {
+        console.log("user creating user : " + error);
+    }
+})
+
+// get : 
+
+app.get('/users',async(req,res)=>{
+    try {
+        const allUser = await userData.find()
+        res.send(allUser)
+    } catch (error) {
+        console.log("user getting user : " + error);
+    } 
+    })
+
+// update : 
+app.put('/users/:id',async(req,res)=>{
+    try {
+        const id = req.params.id
+        const {name,email,mobileNum} = req.body
+        await userData.findByIdAndUpdate(id,{name,email,mobileNum})
+        res.send({message:"user Updated"})
+    } catch (error) {
+        console.log("user updating user : " + error);
+    }
+})
+
+// delete 
+app.delete('/users/:id',async(req,res)=>{
+    try {
+        const id = req.params.id
+        await userData.findByIdAndDelete(id)
+        res.send({message:"user deleted"})
+    } catch (error) {
+        console.log("user deleting user : " + error);
+    }
+})
+
+
 
 const port = process.env.PORT
-
-let userDetails = [
-    {id:1,name:"pravin",email:"pravin@gmail.com"},
-    {id:2,name:"vasu",email:"vasu@gmail.com"}
-]
 
 // get homepage request
 
@@ -19,52 +99,7 @@ app.get('/',(req,res)=>{
     res.send("hello backend app")
 })
 
-// get all users request
 
-app.get('/users',(req,res)=>{
-    res.send(userDetails)
-})
-
-// get selectedUser  request
-
-app.get('/users/:id',(req,res)=>{
-    const id = req.params.id
-    const selectedUser= userDetails.filter((e)=> e.id == id )
-    res.send(selectedUser)
-})
-
-// post request
-
-app.post('/postUser',(req,res)=>{
-    userDetails.push(req.body)
-    res.send({message:"user created"})
-})
-
-// edit request :
-
-app.put('/editUser',(req,res)=>{
-    for(let i = 0; i < userDetails.length;i++){
-        if(req.body.id == userDetails[i].id){
-            userDetails[i].name =req.body.name;
-            userDetails[i].email = req.body.email
-        }
-    }
-    res.send({message:"user updated"})
-})
-
-// delete request
-
-app.delete('/user/:id',(req,res)=>{
-    const id = req.params.id
-    let index
-    for(i=0;i<userDetails.length;i++){
-        if(id == userDetails[i].id){
-            index = i
-        }
-    }
-    userDetails.splice(index,1)
-    res.send(userDetails)
-})
 
 // listening port
 app.listen(5000,()=>{
@@ -72,7 +107,9 @@ app.listen(5000,()=>{
 })
 
 // npm init -> index.js file create 
-// npm i express , dotenv , bodyparser
+// npm i express , dotenv , bodyparser , cors()
+
+// mongoDB - 
 
 
 
